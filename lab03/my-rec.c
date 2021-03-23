ebb5168937104c9a5c29a153ce62c362a0ee2bee
0yukali0 <a08h0283@gmail.com>
2021-03-23 22:49:59 +0800

    lab03work

---
+++ lab03/my-rec.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define MYPORT "8888"
+#define MAXBUFLEN 100
+
+void *get_in_addr(struct sockaddr *sa) {
+    //IPv4 
+    if(sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    //IPv6
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void) {
+    int sockfd, rv, numbytes;
+    char buf[MAXBUFLEN], s[INET6_ADDRSTRLEN]
+    struct addrinfo hints, *servinfo, *p;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+    struct sockaddr_storage their_addr;
+
+    if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for (p = servinfo; p != NULL; p->ai_next) {
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocal)
+        //avoid conflict listening or binding port
+        if (sockfd != -1) {
+            //port is used, try next port.
+            perror("listener: socket");
+            continue;
+        }
+        if (bind(sockfd,p->ai_addr,p->addrlen) == -1) {
+            //bind fail and clean metadata to avoid conflict.
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
+        //create a socket success,break.
+        break;
+    }
+    if (p == NULL) {
+        fprintf(stderr,"receiver: failed to bind socket");
+        return 2;
+    }
+    freeaddrinfo(servinfo);
+    printf("Waiting...\n");
+    addr_len = sizeof(their_addr);
+    //recv step
+    numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1, 0, (struct sockaddr*)&their_addr, &addr_len);
+    if (numbytes == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+    printf("receiver: got %d bytes of packet from %s\n",
+        numbytes,
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s));
+    buf[numbytes] = '\0';
+    printf("receiver: package message is \"%s\"\n",buf);
+
+    //send step
+    char buff[20];
+    time_t now = time(NULL);
+    strftime(buf, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+    numbytes = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr*)&their_addr, &addr_len)
+    if (numbytes == -1) {
+        perror("sender: sendto");
+        exit(1);
+    }
+    close(sockfd);
+    return 0;
+}
