9463fb576a958291db75361ac6f33429fbc11868
0yukali0 <a08h0283@gmail.com>
2021-05-20 16:59:33 +0800

    lab06

---
+++ strcliselect02.c
@@ -0,0 +1,52 @@
+#include	"unp.h"
+#include <sys/time.h>
+#include <unistd.h>
+void
+str_cli(FILE *fp, int sockfd)
+{
+	int			maxfdp1, stdineof;
+	fd_set		rset;
+	char		buf[MAXLINE];
+	int		n;
+	struct timeval tv;
+	tv.tv_sec = 60;
+	tv.tv_usec = 0;
+
+	stdineof = 0;
+	FD_ZERO(&rset);
+	for ( ; ; ) {
+		if (stdineof == 0)
+			FD_SET(fileno(fp), &rset);
+		FD_SET(sockfd, &rset);
+		maxfdp1 = max(fileno(fp), sockfd) + 1;
+		Select(maxfdp1, &rset, NULL, NULL, &tv);
+
+		if (FD_ISSET(sockfd, &rset)) {	/* socket is readable */
+			if ( (n = Read(sockfd, buf, MAXLINE)) == 0) {
+				if (stdineof == 1)
+					return;		/* normal termination */
+				else
+					err_quit("str_cli: server terminated prematurely");
+			} else {
+				Write(sockfd,"60",strlen("60"));
+				return 0;
+			}
+
+			Write(fileno(stdout), buf, n);
+		}
+
+		if (FD_ISSET(fileno(fp), &rset)) {  /* input is readable */
+			if ( (n = Read(fileno(fp), buf, MAXLINE)) == 0) {
+				stdineof = 1;
+				Shutdown(sockfd, SHUT_WR);	/* send FIN */
+				FD_CLR(fileno(fp), &rset);
+				continue;
+			} else {
+				Write(sockfd, "60", strlen("60"));
+				return 0;
+			}
+
+			Writen(sockfd, buf, n);
+		}
+	}
+}
